bcab36c40861efb74cd7074ad20e4226f264abe5
❛ .𝁘ໍ𝛅 𝛕 ⋏ ᰻⃪᱂ 𐌺 𖡛̶ ̶ ̶᪳ ̶ <96779742+Ur-StaRk@users.noreply.github.com>
2025-05-08 16:45:16 +0530

    Update wishcute.py

--- SHUKLAMUSIC/plugins/Yumi/wishcute.py
+++ SHUKLAMUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SHUKLAMUSIC import app 
 
-SUPPORT_CHAT = "aspirantDiscuss"
+SUPPORT_CHAT = "bindaasbuddies"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
@@ -43,7 +43,7 @@ async def cute(_, message):
 
     mention = f"[{user_name}](tg://user?id={str(user_id)})"
     mm = random.randint(1, 100)
-    CUTE = f"🍑 {mention} {mm}% ᴄᴜᴛᴇ ʙᴀʙʏ🥀"
+    CUTE = f"👀 {mention} {mm}% ᴄᴜᴛᴇ ʙᴀʙʏ🥀"
 
     await app.send_document(
         chat_id=message.chat.id,
